c8e1ecae798e338852d10316c20dfc6fa98c1bb4
DenGun17 <den.gun1706@gmail.com>
2023-09-26 21:03:28 +0700

    Menambahkan pekerjaan

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -1,11 +1,13 @@
-<?php 
+<?php
 class Contoh1 extends CI_Controller
 {
-    public function index(){
+    public function index()
+    {
         echo "<h1> Perkenalkan </h1>";
         echo "Nama Saya Deni Gunawan 
         Saya tinggal di daerah Cengkareng
-        Olah raga yang saya sukai adalah Sepakbola";    
+        Olah raga yang saya sukai adalah Sepakbola";
+
+        echo "Saya seorang programmer";
     }
-    
 }
