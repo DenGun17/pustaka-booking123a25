befdcf047d493d3cbcc44d625d9c398361f08cf9
DenGun17 <den.gun1706@gmail.com>
2023-09-26 20:46:59 +0700

    Membuat project CI 3 Pustaka Booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php 
+class Contoh1 extends CI_Controller
+{
+    public function index(){
+        echo "<h1> Perkenalkan </h1>";
+        echo "Nama Saya Deni Gunawan 
+        Saya tinggal di daerah Cengkareng
+        Olah raga yang saya sukai adalah Sepakbola";    
+    }
+    
+}
